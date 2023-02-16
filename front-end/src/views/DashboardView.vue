238e4f0bde92a0594dcafd03e6ce8a0c99c61037
dnsrodrigues <iran.dns@hotmail.com>
2023-02-16 15:48:58 -0300

    primeiro commit

---
+++ front-end/src/views/DashboardView.vue
@@ -0,0 +1,13 @@
+<template>
+
+  <h2>Dashboard</h2>
+  
+</template>
+
+<script setup>
+
+</script>
+
+<style>
+
+</style>
---
+++ front-end/src/views/LoginView.vue
@@ -0,0 +1,28 @@
+<template>
+<div>
+  <h2>Login</h2>
+
+  <form @submit.prevent="Login">
+  
+    <input type="text" placeholder="Seu email">
+    <input type="password" placeholder="Sua Senha">
+    <button type="submit">Login</button>
+
+  </form>
+  
+</div>
+  
+</template>
+
+<script setup>
+
+function Login(){
+  alert('Olá')
+}
+
+</script>
+
+
+<style>
+
+</style>
